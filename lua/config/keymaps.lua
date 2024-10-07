f6fb5f4996b2077bf567eae088bde51ec71ece6d
Muhammad Muaz <21100231@lums.edu.pk>
2024-10-07 02:37:48 -0500

    Added snipe.lua for buffer switching

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "gb", function()
+  local toggle = require("snipe").create_buffer_menu_toggler({
+    -- Limit the width of path buffer names
+    max_path_width = 1,
+  })
+  toggle()
+end, { desc = "[P]Snipe" })
